f1c4e60d58a136f49ba0bc1cd2c1af872c178081
Thomas Lewis <tlewis@training.local>
2020-11-17 13:08:05 -0500

    demo 11 done

--- FortisureMart-Template/Controller/form-submit.php
+++ FortisureMart-Template/Controller/form-submit.php
@@ -1,57 +1,98 @@
 <?php
 
-//check that post items are set
-if(isset($_POST['submit-contact-form']))
-
-{
+//check to see if the customer creation from was just submitted
+if (isset($_POST['submit-contact-form'])) {
     require './Model/query-customer.php';
 
-    $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
-  // variables
-    $firstName = trim(htmlentities($POST['firstName']));
-    $lastName = trim(htmlentities($POST['lastName']));
-    $address = trim(htmlentities($POST['address']));
-    $city = trim(htmlentities($POST['city']));
-    $state = trim(htmlentities($POST['state']));
-    $postalCode = trim(htmlentities($POST['postalCode']));
-    $country = trim(htmlentities($POST['country']));
-    $email = trim(htmlentities($POST['email']));
-    $phone = trim(htmlentities($POST['phone']));
-    $comments = trim(htmlentities($POST['comments']));
-  // variables
+  // Used to remove special encoded characters
+  // https://www.w3schools.com/php/filter_sanitize_string.asp
+  $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
 
-    $customerData = [
-      "firstName" => $firstName,
-      "lastName"  => $lastName,
-      "address" => $address,
-      "city"  => $city,
-      "state" => $state,
-      "postalCode"  => $postalCode,
-      "country"  => $country,
-      "email"  => $email,
-      "phone" => $phone,
-      "comments" => $comments
-    ];
+  // Variables
+    // Customer Contact Info
+      // Trim removes spaces around data example: ' nameHere ' TO-> 'nameHere'
+      // htmlentities changes special character like: 
+        // '&' TO '&amp;'
+        // '<' TO '&lt;'
+        // '"' TO '&quot;'
+      // and by adding single quotes around the entire string
+      // which this helps protect against SQL injection attacks
+      $firstName = trim(htmlentities($POST['firstName']));
+      $lastName = trim(htmlentities($POST['lastName']));
+      $address = trim(htmlentities($POST['address']));
+      $city = trim(htmlentities($POST['city']));
+      $state = trim(htmlentities($POST['state']));
+      $postalCode = trim(htmlentities($POST['postalCode']));
+      $country = trim(htmlentities($POST['country']));
+      $email = trim(htmlentities($POST['email']));
+      $phone = trim(htmlentities($POST['phone']));
+      $comments = trim(htmlentities($POST['comments']));
+    // Customer Contact Info
+  // Variables
+  
+  // Customer array & db insert
+    /* 
+      Creates an array that stores gathered information from the form
+      and send that array to the query-customer.php page to insert to db
+    */
+      $customerData = [
+        "firstName" => $firstName,
+        "lastName" => $lastName,
+        "address" => $address,
+        "city" => $city,
+        "state" => $state,
+        "postalCode" => $postalCode,
+        "country" => $country,
+        "email" => $email,
+        "phone" => $phone,
+        "comments" => $comments
+      ];
+    // Customer Info Array
 
-    $customer = new Customer($db);
-    $customerPurchaseId = $customer->addCustomer($customerData);
-    //display alert to thank customer
-    echo "
-    
-        <script>
-        
-          alert('Thank you {$firstName} {$lastName} for submitting your message.' +
-                'We will review it shortly! Your Id is {$customerPurchaseId}. Your email is : {$email} ');
+    // Insert info to db
+      $customer = new Customer($db);
+      $customerPurchaseId = $customer->addCustomer($customerData);
+    // Insert info to db
+  // Customer array & db insert  
 
-        </script>   
-
-        ";
-
-
-        //database interaction
-
+  //display an alert thanking the customer for their account creations
+  echo "
+  <script>        
+      alert('Thank you {$firstName} {$lastName} for creating an account. We will review it shortly! Your ID is {$customerPurchaseId} ');
+  </script>";
+ 
 }
 
 
 
-?>
+//check if shopping cart was just purchased
+if (isset($_POST['cartCustomerId']))
+{
+  //include query class
+  require './Model/query-cart.php';
+
+  //get entered name and id
+  $enteredId = $_POST['cartCustomerId'];
+  $enteredName = $_POST['cartCustomerName'];
+
+  //create query cart object
+  $cart = new Cart($db);
+
+  //check if id is in database
+  if($cart->checkId($enteredName, $enteredId) && $_SESSION['cart'] != array())
+  {
+    //get session info (Note the session was already started in update-cart.php)
+    $cartArray = $_SESSION['cart'];
+
+    //checkout
+    $cart->checkout($enteredId, $cartArray);
+
+  }
+  else if($_SESSION['cart'] != array())
+  {
+    echo "<script>alert('Account not found!')</script>";
+  }
+
+
+
+}
--- FortisureMart-Template/Model/query-cart.php
+++ FortisureMart-Template/Model/query-cart.php
@@ -1,15 +1,105 @@
 <?php
-class Cart{
+class Cart {
+
+  // Constructor for db connection
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+
+  //query whether an id exists
+  public function checkId($sentName, $sentId) 
+  {
+    //Prepare query
+    $query = "SELECT * FROM customers
+                    WHERE contactFirstName = '$sentName' AND customerNumber = $sentId;";
+    //Prepare query
+
+    // prepare query statement
+    $results = $this->conn->prepare( $query );
+
+    // Execute
+    $results->execute();
+
+    //return true if any row was found
+    if($results->rowCount() > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+  }
+
+  //insert user's transaction to the appropriate tables
+  public function checkout($sentId, $cartArray)
+  {
+
+    //get date
+    $today = date("Y-m-d");
+
+    //Query for transactions table
+        // Prepare Query
+        $query = "INSERT INTO transactions 
+            (orderDate, employeeNumber, customerNumber, status) 
+            VALUES ('$today', NULL, '$sentId', 'Completed');
+        ";
+        // Prepare Query
+
+        // prepare query statement
+        $results = $this->conn->prepare( $query );
+
+        // Execute
+        $results->execute();
+    //Query for transactions table
+
+
+
+    //get transaction number
+    $transactionNum = $this->conn->lastInsertId();
+
+    //Query for transactiondetails table
     
+        // Prepare Query
+        $query = "INSERT INTO transactiondetails 
+            (transactionNumber, productCode, quantityPurchased) 
+            VALUES ";
+
+        //get length of array
+        $cartLen = count($cartArray);
+
+        //Append record for each product
+        for($index = 0; $index < $cartLen; $index++)
+        {
+            //variable for array info
+            $prodCode = $cartArray[$index]['code'];
+            $prodQty = $cartArray[$index]['qty'];
+
+            //append values to query
+            $query = $query . "('$transactionNum', '$prodCode', '$prodQty')";
+
+            //add comma on each except for last
+            if($index < $cartLen - 1)
+            {
+                //add comma
+                $query = $query . ", ";
+            }
+
+        }
+        // Prepare Query
+
+        // prepare query statement
+        $results = $this->conn->prepare( $query );
+
+        // Execute
+        $results->execute();
+    //Query for transactions table
+
+    //clear cart session
+    $_SESSION['cart'] = array();
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
 ?>
